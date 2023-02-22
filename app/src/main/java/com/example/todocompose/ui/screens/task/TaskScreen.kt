eea9039835084033ad6c62694c26c37fca5e3dcf
ripinpip <thanebhickman@gmail.com>
2023-02-22 12:18:05 -0700

    Added Field Validations

--- app/src/main/java/com/example/todocompose/ui/screens/task/TaskScreen.kt
+++ app/src/main/java/com/example/todocompose/ui/screens/task/TaskScreen.kt
@@ -1,24 +1,72 @@
 package com.example.todocompose.ui.screens.task
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import com.example.todocompose.data.models.Priority
 import com.example.todocompose.data.models.ToDoTask
+import com.example.todocompose.ui.viewmodels.SharedViewModel
 import com.example.todocompose.util.Action
 
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
 @Composable
 fun TaskScreen(
     selectedTask: ToDoTask?,
+    sharedViewModel: SharedViewModel,
     navigateToListScreen: (Action) -> Unit
 ) {
-    
+    val title: String by sharedViewModel.title
+    val description: String by sharedViewModel.description
+    val priority: Priority by sharedViewModel.priority
+
+    val context = LocalContext.current
+
     Scaffold(
         topBar = {
             TaskAppBar(
                 selectedTask = selectedTask,
-                navigateToListScreen = navigateToListScreen
+                navigateToListScreen = { action ->
+                    if (action == Action.NO_ACTION) {
+                        navigateToListScreen(action)
+                    }
+                    else {
+                        if (sharedViewModel.validateFields()) {
+                            navigateToListScreen(action)
+                        }
+                        else {
+                            displayToast(context = context)
+                        }
+                    }
+                }
             )
         },
-        content = {}
+        content = {
+            TaskContent(
+                title = title,
+                onTitleChange = {
+                    sharedViewModel.updateTitle(it)
+                },
+                description = description,
+                onDescriptionChange = {
+                    sharedViewModel.description.value = it
+                },
+                priority = priority,
+                onPrioritySelected = {
+                    sharedViewModel.priority.value = it
+                }
+            )
+        }
     )
-    
+}
+
+fun displayToast(context: Context) {
+    Toast.makeText(
+        context,
+        "Fields Empty.",
+        Toast.LENGTH_SHORT
+    ).show()
 }
--- app/src/main/java/com/example/todocompose/ui/viewmodels/SharedViewModel.kt
+++ app/src/main/java/com/example/todocompose/ui/viewmodels/SharedViewModel.kt
@@ -4,8 +4,10 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.todocompose.data.models.Priority
 import com.example.todocompose.data.models.ToDoTask
 import com.example.todocompose.data.repositories.ToDoRepository
+import com.example.todocompose.util.Constants.MAX_TITLE_LENGTH
 import com.example.todocompose.util.RequestState
 import com.example.todocompose.util.SearchAppBarState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -19,6 +21,11 @@ import javax.inject.Inject
 class SharedViewModel @Inject constructor(
     private val repository: ToDoRepository
 ): ViewModel() {
+
+    val id: MutableState<Int> = mutableStateOf(0)
+    val title: MutableState<String> = mutableStateOf("")
+    val description: MutableState<String> =  mutableStateOf("")
+    val priority: MutableState<Priority> = mutableStateOf(Priority.LOW)
 
     val searchAppBarState: MutableState<SearchAppBarState> =
         mutableStateOf(SearchAppBarState.CLOSED)
@@ -51,6 +58,30 @@ class SharedViewModel @Inject constructor(
                 _selectedTask.value = task
             }
         }
+    }
+
+    fun updateTaskFields(selectedTask: ToDoTask?) {
+        if (selectedTask != null) {
+            id.value = selectedTask.id
+            title.value = selectedTask.title
+            description.value = selectedTask.description
+            priority.value = selectedTask.priority
+        } else {
+            id.value = 0
+            title.value = ""
+            description.value = ""
+            priority.value = Priority.LOW
+        }
+    }
+
+    fun updateTitle(newTitle: String) {
+        if (newTitle.length < MAX_TITLE_LENGTH) {
+            title.value = newTitle
+        }
+    }
+
+    fun validateFields() : Boolean {
+        return title.value.isNotEmpty() && description.value.isNotEmpty()
     }
 
 }
